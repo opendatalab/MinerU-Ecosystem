4026ec049e6ee2f318b58bd4321a82de64dcc6fc
yangqi <yangqi@pjlab.org>
2026-03-19 17:30:31 +0800

    Update default flash base URL for release

--- sdk/python/src/mineru/_constants.py
+++ sdk/python/src/mineru/_constants.py
@@ -2,5 +2,4 @@
 
 DEFAULT_BASE_URL = "https://mineru.net/api/v4"
 
-# TODO(release): 上线前换回 https://mineru.net/api/v1/agent
-DEFAULT_FLASH_BASE_URL = "https://staging.mineru.org.cn/api/v1/agent"
+DEFAULT_FLASH_BASE_URL = "https://mineru.net/api/v1/agent"
--- sdk/typescript/src/constants.ts
+++ sdk/typescript/src/constants.ts
@@ -1,4 +1,3 @@
 export const DEFAULT_BASE_URL = "https://mineru.net/api/v4";
 
-// TODO(release): 上线前换回 https://mineru.net/api/v1/agent
-export const DEFAULT_FLASH_BASE_URL = "https://staging.mineru.org.cn/api/v1/agent";
+export const DEFAULT_FLASH_BASE_URL = "https://mineru.net/api/v1/agent";
