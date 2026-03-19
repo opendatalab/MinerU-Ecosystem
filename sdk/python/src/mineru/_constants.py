d9fc6ed93b113aa907817b9a425c8b0543e8b9f8
yangqi <yangqi@pjlab.org>
2026-03-19 14:31:00 +0800

    Add shared constants for default base URLs in Python and TypeScript SDKs

---
+++ sdk/python/src/mineru/_constants.py
@@ -0,0 +1,6 @@
+"""Shared SDK constants."""
+
+DEFAULT_BASE_URL = "https://mineru.net/api/v4"
+
+# TODO(release): 上线前换回 https://mineru.net/api/v1/agent
+DEFAULT_FLASH_BASE_URL = "https://staging.mineru.org.cn/api/v1/agent"
